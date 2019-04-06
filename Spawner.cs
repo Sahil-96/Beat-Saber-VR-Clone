49a178ba3c62509c134002da3efbeded1746e3a1
Sahil-96 <Sahil_Mann@msn.com>
2019-04-05 19:52:44 -0700

    The script that spawns the cubes

--- Spawner.cs
+++ Spawner.cs
@@ -2,12 +2,15 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+//this script will spawn the cubes to break by the sabers based on the BMP of the preselected song
+
 public class Spawner : MonoBehaviour
 {
-    public GameObject[] cubes;
-    public Transform[] points;
-    public float beat = (60/105)*2;
-    private float timer;
+    public GameObject[] cubes; // an array to hold all the cube game objects
+    public Transform[] points; // an array of possible spawning locations for the cubes
+    public float beat = (60/105)*2; // the point in time when the cubes spawn
+    private float timer; // a variable the will count up as the song progresses. 
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,12 +20,12 @@ public class Spawner : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (timer > beat)
+        if (timer > beat) // if the counter has reached the beat in the song, a cube will spawn
         {
-            GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0,4)]);
+            GameObject cube = Instantiate(cubes[Random.Range(0, 2)], points[Random.Range(0,4)]); // spawn cube at semi random location 
             cube.transform.localPosition = Vector3.zero;
-            cube.transform.Rotate(transform.forward, 90 * Random.Range(0,4));
-            timer -= beat;
+            cube.transform.Rotate(transform.forward, 90 * Random.Range(0,4)); // random rotation
+            timer -= beat; // reset the timer
         }
 
         timer += Time.deltaTime;
