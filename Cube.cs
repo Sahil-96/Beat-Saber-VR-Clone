6e0e695b48707833b74f36ca87e1d68511cf422e
Sahil-96 <Sahil_Mann@msn.com>
2019-04-05 19:46:12 -0700

    Create Cube.cs

---
+++ Cube.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// this is a basic script that just moves the breakable cubes from the spawner area towards the player 
+
+public class Cube : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position += Time.deltaTime * 2 * transform.forward; //the *2 is because it then matchs the BPM and the beats counter in spawner script
+    }
+}
